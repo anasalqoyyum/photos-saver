b16e2f49fb94d7d0940c7e7235d87e7ba7afdc40
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2026-03-10 00:56:50 +0700

    chore: enable backend mode by default

--- src/backend-config.ts
+++ src/backend-config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_MODE_ENABLED = false
-export const BACKEND_BASE_URL = ''
+export const BACKEND_MODE_ENABLED = true
+export const BACKEND_BASE_URL = 'https://photos-saver.anasalqoyyum.dev'
